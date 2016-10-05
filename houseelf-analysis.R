5d8d4010a9961a7e57a7709a4950e36eb983e294
Brandon Vaverka <Brandon Vaverka>
2016-10-05 12:53:51 -0500

    This calculates the GC-content of a sequence regardless of the cpitalization of the sequence

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -1,2 +1,11 @@
 # This will analysis house elves
+library("stringr")
 elf_dna <- read.csv("./data/houseelf_earlength_dna_data_1.csv")
+
+gc_content <- function(dna) { 
+  Gs <- str_count(dna, "g")
+  Cs <- str_count(dna, "c")
+  gc <- round((Gs + Cs) / str_length(dna) * 100, digits = 2)
+  return(gc)
+  }
+print(gc_content(str_to_lower(elf_dna$dnaseq))) 
